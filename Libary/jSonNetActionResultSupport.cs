3afbc82e87cff95e339d0b8b96b1278120858fd8
Brian Webb <BrianMinister@users.noreply.github.com>
2015-08-29 14:06:02 -0700

    Copied from the NewtonSoft json.net website.
    I added a few minor tweaks to make sure that my implementation would be very light.

---
+++ Libary/jSonNetActionResultSupport.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Text;
+
+namespace System.Web.Mvc
+{
+
+    public class JsonNetResult : ActionResult
+    {
+
+        public Encoding ContentEncoding { get; set; }
+        public string ContentType { get; set; }
+        public object Data { get; set; }
+        public JsonSerializerSettings SerializerSettings { get; set; }
+        public Formatting Formatting { get; set; }
+
+        public JsonNetResult()
+        {
+            SerializerSettings = new JsonSerializerSettings();
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+
+            HttpResponseBase response = context.HttpContext.Response;
+
+            response.ContentType = !string.IsNullOrEmpty(ContentType)
+              ? ContentType
+              : "application/json";
+
+            if (ContentEncoding != null)
+                response.ContentEncoding = ContentEncoding;
+
+            if (Data != null)
+            {
+                JsonTextWriter writer = new JsonTextWriter(response.Output) { Formatting = Formatting };
+                JsonSerializer serializer = JsonSerializer.Create(SerializerSettings);
+                serializer.Serialize(writer, Data);
+                writer.Flush();
+            }
+
+        }
+
+    }
+
+    public static class ControllerExtender
+    {
+        public static JsonNetResult JsonNet
+          (this Controller ctrl, object data, 
+           Formatting formatting = Formatting.Indented) 
+        { 
+            JsonNetResult ret = new JsonNetResult();
+            ret.Formatting = formatting;
+            ret.Data = data;
+            return ret;
+        }
+    }
+
+}
